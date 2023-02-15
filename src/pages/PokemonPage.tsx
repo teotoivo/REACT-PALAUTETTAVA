7d965c3fd4e0187ac76c3a0a93f2ec7e054935e4
teotoivo <teo.maximilien@gmail.com>
2023-02-15 17:00:17 +0200

    b

--- src/pages/PokemonPage.tsx
+++ src/pages/PokemonPage.tsx
@@ -13,9 +13,17 @@ import {
   EvolutionChain,
 } from "pokenode-ts";
 import PopUp from "../components/PopUp";
+import { Link } from "react-router-dom";
 
+//create a new type which extednds the EvoultionChain type and adds a new property to it
+type EvoultionChainExtended = EvolutionChain["chain"] & {
+  //add a pokemon
+  pokemon: Pokemon;
+  isLast: boolean;
+};
 export default function PokemonPage() {
   const evApi = new EvolutionClient();
+  const api = new PokemonClient();
   const params = useParams();
   const [loaded, setLoaded] = useState(false);
   const [data, setData] = useState<Pokemon | null>(null);
@@ -24,22 +32,28 @@ export default function PokemonPage() {
   const [error, setError] = useState<Error | null>(null);
   const [species, setSpecies] = useState<PokemonSpecies | null>(null);
   const [evoulution, setEvolution] = useState<EvolutionChain | null>(null);
-  const [chain, setChain] = useState<any>(null);
+  const [chain, setChain] = useState<Array<EvoultionChainExtended> | null>(
+    null
+  );
 
-  function extractEvolutionChains(pokemonData: any) {
-    const chains: EvolutionChain[] = [];
+  async function extractEvolutionChains(pokemonData: EvolutionChain) {
+    const chainsRes: EvoultionChainExtended[] = [];
+    let running = true;
+    let p = pokemonData.chain;
+    //extract all evolutions of pokemon into an array
+    while (running) {
+      const res = await api.getPokemonByName(p.species.name);
 
-    if (pokemonData.chain.evolves_to.length > 0) {
-      function traverseChain(chain: any) {
-        chains.push(chain);
-        chain.evolves_to.forEach((evolution2: any) =>
-          traverseChain(evolution2)
-        );
+      if (p.evolves_to.length === 0) {
+        chainsRes.push({ ...p, pokemon: res, isLast: true });
+        running = false;
+      } else {
+        chainsRes.push({ ...p, pokemon: res, isLast: false });
+        p = p.evolves_to[0];
       }
-      traverseChain(pokemonData.chain);
     }
 
-    setChain(chains);
+    return chainsRes;
   }
 
   useEffect(() => {
@@ -52,11 +66,15 @@ export default function PokemonPage() {
         const evres = await fetch(spec.evolution_chain.url);
         const ev: EvolutionChain = await evres.json();
 
-        extractEvolutionChains(ev);
+        const c = await extractEvolutionChains(ev);
 
+        if (c.length !== 0) {
+          setChain(c);
+        }
         setEvolution(ev);
         setSpecies(spec);
         setData(res);
+
         setLoaded(true);
       } catch (e: any) {
         setError(e);
@@ -110,7 +128,7 @@ export default function PokemonPage() {
                   })[0]
                   .flavor_text.replace(/(\r\n|\n|\r)/gm, " ")}
               </p>
-              <div className="flex bg-slate-700 w-4/5 gap-6 rounded-md p-6 max-[400px]:flex-col">
+              <div className="flex bg-slate-700 w-4/5 gap-6 rounded-md p-6 max-[400px]:flex-col flex-wrap">
                 <div>
                   <p className="flex text-2xl">
                     Height: {Number(data?.height) / 10}m
@@ -143,46 +161,47 @@ export default function PokemonPage() {
                     </div>
                   </div>
                 </div>
-              </div>
-              <div className="flex flex-wrap gap-4">
-                <div>
-                  <p className="flex text-2xl">Types:&nbsp;</p>
-                  <div className="flex gap-3">
-                    {data?.types.map((x) => {
-                      return (
-                        <div
-                          key={x.type.name}
-                          className="flex flex-row rounded-lg p-1 px-2 bg-slate-800 text-white"
-                        >
-                          <p className="text-2xl">{x.type.name}</p>
-                        </div>
-                      );
-                    })}
+
+                <div className="flex flex-wrap gap-4">
+                  <div>
+                    <p className="flex text-2xl">Types:&nbsp;</p>
+                    <div className="flex gap-3 flex-wrap">
+                      {data?.types.map((x) => {
+                        return (
+                          <div
+                            key={x.type.name}
+                            className="flex flex-row rounded-lg p-1 px-2 bg-slate-800 text-white"
+                          >
+                            <p className="text-2xl">{x.type.name}</p>
+                          </div>
+                        );
+                      })}
+                    </div>
                   </div>
-                </div>
-                <div>
-                  <p className="dlex text-2xl">Weaknesses:&nbsp;</p>
-                  <div className="flex gap-3">
-                    {data?.types.map((x) => {
-                      return (
-                        <div
-                          key={x.type.name}
-                          className="flex flex-row rounded-lg p-1 px-2 bg-slate-800 text-white"
-                        >
-                          <p className="text-2xl">{x.type.name}</p>
-                        </div>
-                      );
-                    })}
+                  <div>
+                    <p className="dlex text-2xl">Weaknesses:&nbsp;</p>
+                    <div className="flex gap-3">
+                      {data?.types.map((x) => {
+                        return (
+                          <div
+                            key={x.type.name}
+                            className="flex flex-row rounded-lg p-1 px-2 bg-slate-800 text-white"
+                          >
+                            <p className="text-2xl">{x.type.name}</p>
+                          </div>
+                        );
+                      })}
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <div className="w-full flex items-center justify-between">
+          <div className="w-full flex items-center justify-around flex-wrap">
             {data?.stats.map((x) => {
               return (
                 <div className="flex flex-col items-center" key={x.stat.name}>
-                  <p className="text-2xl">{x.stat.name}</p>
+                  <p className="text-2xl break-words">{x.stat.name}</p>
                   <div className="flex flex-col items-center h-64">
                     <div className="border-black border-2 h-full w-4/5 flex">
                       <div
@@ -199,7 +218,57 @@ export default function PokemonPage() {
               );
             })}
           </div>
-          <div className="w-full flex">{JSON.stringify(evoulution)}</div>
+          <div className="w-full flex">
+            <div className="flex w-full justify-around items-center">
+              {(() => {
+                function content() {
+                  return chain?.map((x) => {
+                    return (
+                      <>
+                        <Link
+                          to={"/pokemon/" + x.species.name}
+                          className="w-1/6"
+                          key={x.species.name}
+                        >
+                          <div
+                            className={(() => {
+                              let style = "rounded-full border-2 w-full";
+
+                              if (x.species.name == data?.name) {
+                                return style + " bg-slate-700";
+                              } else return style;
+                            })()}
+                          >
+                            <img
+                              className="w-full"
+                              src={x.pokemon.sprites.front_default!}
+                              alt="pokemon picture"
+                            />
+                          </div>
+                          <div className="relative flex bg-slate-700 bg-opacity-80 w-min left-1/2 -translate-x-1/2 p-2 rounded-2xl px-4 text-white font-semibold">
+                            {x.species.name}
+                          </div>
+                        </Link>
+                        <div>
+                          {(() => {
+                            if (x.isLast) {
+                              return <div className=""></div>;
+                            } else {
+                              return (
+                                <div className="w-1/6 text-9xl">&#8594;</div>
+                              );
+                            }
+                          })()}
+                        </div>
+                      </>
+                    );
+                  });
+                }
+
+                return content();
+              })()}
+            </div>
+          </div>
         </div>
       </div>
       <PopUp
